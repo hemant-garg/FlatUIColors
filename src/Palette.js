5df6db27d94d0fa63d9afc4f9e3abe9f1a215c15
Hemant <hemant.gr6@gmail.com>
2019-05-22 06:38:48 +0530

    add format select

--- src/Palette.js
+++ src/Palette.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 import './Palette.css';
+import Navbar from './Navbar';
 class Palette extends Component {
 	state = {
-		level: 500
+		level: 500,
+		format: 'hex'
 	};
 
 	changeLevel = (level) => {
 		this.setState({ level });
 	};
 
+	changeFormat = (format) => {
+		this.setState({ format });
+	};
+
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state;
-		const colorboxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
+		const { level, format } = this.state;
+		const colorboxes = colors[level].map((color) => <ColorBox background={color[format]} name={color.name} />);
 		return (
 			<div className='Pallete'>
-				<div className='slider'>
-					<Slider defaultValue={level} step={100} min={100} max={900} onAfterChange={this.changeLevel} />
-				</div>
+				<Navbar handleChange={this.changeFormat} level={level} changeLevel={this.changeLevel} />
 				<div className='Pallete-colors'>{colorboxes}</div>
 			</div>
 		);
