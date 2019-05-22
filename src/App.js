fec650e39f720d0344f79aab7c19cbf68fc1fec6
Hemant <hemant.gr6@gmail.com>
2019-05-22 05:32:47 +0530

    add color helper to generate color shades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Pallete from './Pallete';
 import seedColors from './seedColors';
-
+import { generatePalette } from './ColorHelpers';
 class App extends Component {
 	render() {
+		console.log(generatePalette(seedColors[5]));
 		return <Pallete {...seedColors[5]} />;
 	}
 }
