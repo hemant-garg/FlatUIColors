edb16163f8468b2829d9279dc0c7949e4450d650
Hemant <hemant.gr6@gmail.com>
2019-05-22 15:28:54 +0530

    added palette list and minipalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
+
+const styles = {
+	root: {
+		backgroundColor: 'blue',
+		height: '100vh',
+		display: 'flex',
+		alignItems: 'flex-start',
+		justifyContent: 'center'
+	},
+	conatiner: {
+		width: '50%',
+		display: 'flex',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+		flexWrap: 'wrap'
+	},
+	nav: {
+		display: 'flex',
+		width: '100%',
+		justifyContent: 'space-between',
+		color: 'white'
+	},
+	palettes: {
+		boxSizing: 'border-box',
+		width: '100%',
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3, 31%)',
+		gridGap: '3.5%'
+	}
+};
 
 class PaletteList extends Component {
+	goToPalette = (id) => {
+		this.props.history.push(`/palette/${id}`);
+	};
 	render() {
-		const { palettes } = this.props;
+		const { palettes, classes } = this.props;
 		return (
-			<div>
-				<h1>Palette List</h1>
-				{palettes.map((palette) => (
-					<Link to={`/palette/${palette.id}`}>
-						<h1>{palette.paletteName}</h1>
-					</Link>
-				))}
+			<div className={classes.root}>
+				<div className={classes.container}>
+					<nav className={classes.nav}>
+						<h1>Palette List</h1>
+					</nav>
+					<div className={classes.palettes}>
+						{palettes.map((palette) => (
+							<MiniPalette handleClick={() => this.goToPalette(palette.id)} {...palette} />
+						))}
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
